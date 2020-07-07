c7e414bea2e69d8a2f446c373932e7b12f8c5b18
jhsebastian <jhsebastian12@gmail.com>
2020-07-07 00:55:33 -0500

    create the first part of chat page, add component catItem and userItem

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Chat.css';
+import CatItem from './CatItem/CatItem';
+import UserItem from './UserItem/UserItem';
 
 const Chat = () => {
   return(
-    <div>
-      Soy un chat
+    <div className="chatbot-chat-container">
+      <div className="chatbot-chat-content">
+        <div className="chatbot-chat">
+          <div className="chatbot-chat-container-body">
+            <CatItem/>
+            <UserItem/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
